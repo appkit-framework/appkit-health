af16a46637a07f0c6920eecd7f8951414bf2a1bb
appkit-framework <admin@appkit.biz>
2025-12-09 14:00:03 +0100

    Initial commit

---
+++ src/HealthCheckResult.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppKit\Health;
+
+use AppKit\Health\HealthIndicatorInterface;
+
+class HealthCheckResult {
+    private $status;
+    private $details;
+
+    function __construct($data) {
+        $this -> status = true;
+        $this -> details = $this -> parseRecur($data);
+    }
+
+    public function isHealthy() {
+        return $this -> status;
+    }
+
+    public function getDetails() {
+        return $this -> details;
+    }
+
+    private function parseRecur($data) {
+        if(is_array($data)) {
+            if(empty($data))
+                return true;
+
+            foreach($data as $k => $v)
+                $data[$k] = $this -> parseRecur($v);
+
+            return $data;
+        } else if($data instanceof self || $data instanceof HealthIndicatorInterface) {
+            if($data instanceof HealthIndicatorInterface)
+                $data = $data -> checkHealth();
+
+            if(!$data -> isHealthy())
+                $this -> status = false;
+
+            return $data -> getDetails();
+        } else {
+            $data = (bool) $data;
+
+            if(!$data)
+                $this -> status = false;
+
+            return $data;
+        }
+    }
+}
---
+++ src/HealthIndicatorInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace AppKit\Health;
+
+interface HealthIndicatorInterface {
+    public function checkHealth();
+}
